b755e2a003d1746775bcd3cb019390a8a47f56af
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-12-17 20:34:04 +0300

    +Animation +3dGame

---
+++ Animation/Assets/Animate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+
+    Animator m_animator;
+
+    void Start()
+    {
+        m_animator = gameObject.GetComponent<Animator>();
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_animator.SetBool("Go", true);
+            Debug.Log("start");
+        }
+    }
+}
