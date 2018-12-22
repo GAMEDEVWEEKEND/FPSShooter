c80f8002a7613d0bd27ec52ece38b199490d625e
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-12-22 22:03:28 +0300

    +Strategie

---
+++ Strategie/Assets/NavigationScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationScript : MonoBehaviour {
+
+    public Camera cam; //бросаем камеру
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) //если нажата клавиша мыши
+        {
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);//снимаем точку клика
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point); //перемещаем агента в точку
+            }
+        }
+    }
+}
+
---
+++ Strategie/Assets/OrbitCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour {
+
+    public Transform target; //Переменная хранит ссылку на объект, вокруг производиться облет
+    public float rotSpeed = 1.5f;
+    private float _rotY;
+    private Vector3 _offset;
+    // Use this for initialization
+    void Start()
+    {
+        _rotY = transform.eulerAngles.y;
+        _offset = target.position - transform.position; //сохранние начального смещения между камерой и целью
+    }
+    private void LateUpdate()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        if (horInput != 0)//медленный поворот камеры при помощи клавиш со стрелками
+        {
+            _rotY += horInput * rotSpeed;
+        }
+        else
+        {
+            _rotY += Input.GetAxis("Mouse X") * rotSpeed * 3;//или быстрый поворот с помощью мыши
+        }
+        Quaternion rotation = Quaternion.Euler(0, _rotY, 0);
+        transform.position = target.position - (rotation * _offset);//поддерживаем начальное, сдвигаемое в соответствии с поворотом камеры.
+        transform.LookAt(target); //камера всегда направлена на цель, где бы относительно этой цели она ни располагалась
+    }
+}
