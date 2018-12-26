660d1530d314811c8bd830bc8dd2081a175d2c58
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2018-12-26 12:10:11 +0300

    +2DGame

---
+++ 2dGame/Assets/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    [SerializeField] private MemoryCard originalCard;//ссылка для карты в сцене
+    [SerializeField] private Sprite[] images;//массив ссылок на ресурсы-спрайты
+
+    public const int gridRows = 2;//значения, количество ячеей в сетки и их расстояние друг от друга
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    void Start()
+    {
+        Vector3 startPos = originalCard.transform.position;//Положение первой карты, остальные от этой точки
+
+
+
+
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);//вызов открытого метода в MemoryCard
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
